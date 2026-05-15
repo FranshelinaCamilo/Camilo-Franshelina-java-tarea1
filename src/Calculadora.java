c170e57f7307545f9eb29e09e3098eea4bbb474d
Elisandra Camilo <franshelina_camilo@ucne.edu.do>
2026-05-14 23:45:55 -0400

    Crear main y clase calculadora

---
+++ src/Calculadora.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+    //1. Calculadora basica    
+    public static void CalculadoraBasica(){
+
+        Scanner sc = new Scanner(System.in);
+
+        int servicio;
+        double num1, num2;
+
+        System.out.println("\n====== MENU =====");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+
+        System.out.print("\nSeleccione una opción:");
+        servicio = sc.nextInt();
+
+        //Verificacion de servicio seleccionado 
+        if(servicio > 0 && servicio < 5 ){
+            System.out.print("\n|> Digite el primer numero:");
+            num1 = sc.nextInt();
+
+            System.out.print("|> Digite el segundo numero:");
+            num2 = sc.nextInt();
+
+            switch(servicio){
+                case 1:
+                    System.out.printf("|> Resultado: " + suma(num1, num2));
+                    break;
+                case 2:
+                    System.out.printf("|> Resultado: " + resta(num1, num2));
+                    break;
+                case 3:
+                    System.out.printf("|> Resultado: " + multiplicacion(num1, num2));
+                    break;
+                case 4:
+                    System.out.printf("|> Resultado: " + division(num1, num2));
+                    break;
+            }
+        }
+        else{
+               System.out.println("Opcion invalida."); 
+        }
+    }
+
+    public static double suma(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public static double resta(double num1, double num2){
+        return num1 - num2;
+    }
+
+    public static double multiplicacion(double num1, double num2){
+        return num1 * num2;
+    }
+    public static double division(double num1, double num2){
+        if(num2 == 0){
+            System.out.println("\nDivision invalida.");
+        }
+        return num1/num2;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args){
+        menu();
+
+    }
+
+    public static void menu(){
+
+        Scanner sc = new Scanner(System.in);
+
+        int servicio;
+
+        do{
+            System.out.println("\n\n============== MENU ==============");
+            System.out.println("1. Calculadora básica");
+            System.out.println("2. Número par o impar");
+            System.out.println("3. Tabla de multiplicar");
+            System.out.println("4. Contador de vocales");
+            System.out.println("5. Promedio de notas");
+            System.out.println("6. clase estudiante");
+            System.out.println("7. Cuenta bancaria");
+            System.out.println("8. Herencia de vehículos");
+            System.out.println("9. Matriz 3x3");
+            System.out.println("10. Sistema básico de inventario");
+            System.out.println("11. Salir de la aplicación");
+
+            System.out.print("\n|> Seleccione una opción:");
+            servicio = sc.nextInt();
+
+            switch (servicio) {
+                case 1:
+                    Calculadora.CalculadoraBasica();
+                    sc.nextLine();
+                    pause(sc);
+                    break;
+                case 2:
+                    ParOImpar.ParImpar();
+                    sc.nextLine();
+                    pause(sc);
+                    break;
+                case 3: 
+                    TablaMultiplicar.tabla();
+                    sc.nextLine();
+                    pause(sc);
+                    break;
+                case 4:
+                    ContadorVocales.cantVocales();
+                    sc.nextLine();
+                    pause(sc);
+                    break;
+                case 5:
+                    Promedio.promedioNotas();
+                    sc.nextLine();
+                    pause(sc);
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    System.out.println("\nSaliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opcion invalida.");
+            }
+            
+        }while(servicio != 11);
+    }
+
+    public static void pause(Scanner sc){
+        System.out.printf("\nPresione una ENTER para continuar");
+        sc.nextLine();
+    }
+
+}
